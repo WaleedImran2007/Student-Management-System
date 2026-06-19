4ce93021b392b9c59a3eae01640aac3e2832b0c0
Waleed Imran <imrankhalida2009@gmail.com>
2026-06-19 21:46:28 +0500

    Fix production CORS

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -23,7 +23,12 @@ app.use(cors({
         "http://localhost:5173",
         "https://student-management-system-gray-psi.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 
