efaf50d0a8eb8e1f002401bc30814f1cdb7a2733
Waleed Imran <imrankhalida2009@gmail.com>
2026-06-19 15:32:55 +0500

    Add Vercel frontend to CORS

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://student-management-system-gray-psi.vercel.app/",
+        "https://student-management-system-gray-psi.vercel.app",
     ],
 }));
 
