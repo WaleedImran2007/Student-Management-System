91d5e1f497c4c62c8644a18d9483ca6b7780dd96
Waleed Imran <imrankhalida2009@gmail.com>
2026-06-19 21:13:56 +0500

    I am totally fucked up bc

--- sms-backend/utils/sendEmail.js
+++ sms-backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (email, token) => {
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
-        family: 4
+        family: 4,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD,
