557ffaf4884c5489d294589e03ce2ddb9ecab513
Waleed Imran <imrankhalida2009@gmail.com>
2026-06-19 15:21:59 +0500

    Update CORS settings

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -18,7 +18,12 @@ import { authMiddleware } from './middleware/authMiddleware.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173"
+    ],
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
